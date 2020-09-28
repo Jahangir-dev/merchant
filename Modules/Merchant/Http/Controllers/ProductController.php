0d9e45a40d4a0a71de48f4f5af0ea92614fcfeca
devolper102 <muhammad_sikander@outlook.com>
2020-09-29 01:54:07 +0500

    product featured

--- Modules/Merchant/Http/Controllers/ProductController.php
+++ Modules/Merchant/Http/Controllers/ProductController.php
@@ -58,6 +58,7 @@ class ProductController extends Controller
             'special_price' => $request['special_price'],
             'quantity' => $request['quantity'],
             'weight' => $request['weight'],
+            'featured' => 1,
             'user_id' => Auth::id()
         ]);
         $product->categories()->attach($request['category']);
