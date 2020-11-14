e63d27ca5b8ee9c43cb9a6b89cbca6e0a50607e5
devolper102 <muhammad_sikander@outlook.com>
2020-11-14 23:02:21 +0500

    mail template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{asset('frontend/img/logo-small.png')}}" class="logo" alt="Laravel Logo">
+<img src="{{asset('frontend/images/main-logo.png')}}" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
